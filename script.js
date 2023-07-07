67aec6c620315a28c5ebd0ebee6650a2553deabe
NitroluMemoDayi <starbot8279@gmail.com>
2023-07-07 23:38:11 +0300

    version 0.1

---
+++ script.js
@@ -0,0 +1,425 @@
+var hadisler = 
+[
+{
+      hadis: `(Allah Rasûlü) "Din nasihattır/samimiyettir" buyurdu. "Kime Yâ Rasûlallah?" diye sorduk. O da; "Allah'a, Kitabına, Peygamberine, Müslümanların yöneticilerine ve bütün müslümanlara" diye cevap verdi.`,
+      kaynak: `(Müslim, İmân, 95.)`,
+      bg: `#b22222`,
+},
+{
+      hadis: `İslâm, güzel ahlâktır. `,
+      kaynak: `(Kenzü'l-Ummâl, 3/17, HadisNo: 5225. )`,
+      bg: `#b22222`,
+},
+{
+      hadis: `İnsanlara merhamet etmeyene Allah merhamet etmez.`,
+      kaynak: `(Müslim, Fedâil, 66; Tirmizî, Birr, 16. )`,
+      bg: `#b22222`,
+
+},
+{
+      hadis: `Kolaylaştırınız, güçleştirmeyiniz, müjdeleyiniz, nefret ettirmeyiniz.`,
+      kaynak: `(Buhârî, İlm, 12; Müslim, Cihâd, 6. )`,
+      bg: `#b22222`,
+
+},
+{
+      hadis: `İnsanların Peygamberlerden öğrenegeldikleri sözlerden biri de: "Utanmadıktan sonra dilediğini yap!" sözüdür.`,
+      kaynak: `(Buhârî, Enbiyâ, 54; EbuDâvûd, Edeb, 6. )`,
+      bg: `#b22222`,
+
+},
+{
+      hadis: `Hayra vesile olan, hayrı yapan gibidir.`,
+      kaynak: `(Tirmizî, İlm, 14. )`,
+      bg: `#b22222`,
+
+},
+{
+      hadis: `Mümin, bir delikten iki defa sokulmaz.(Mümin, iki defa aynı yanılgıya düşmez)`,
+      kaynak: `(Buhârî, Edeb, 83; Müslim, Zühd, 63. )`,
+      bg: `#b22222`,
+
+},
+{
+      hadis: `Nerede olursan ol Allah'a karşı gelmekten sakın; yaptığın kötülüğün arkasından bir iyilik yap ki bu onu yok etsin. İnsanlara karşı güzel ahlakın gereğine göre davran.`,
+      kaynak: `Tirmizî, Birr, 55. `,
+      bg: `#b22222`,
+
+},
+{
+      hadis: `Allah, sizden birinizin yaptığı işi, ameli ve görevi sağlam ve iyi yapmasından hoşnut olur.`,
+      kaynak: `(Taberânî, el-Mu'cemü'l-Evsat, 1/275; Beyhakî, ?u'abü'l-Îmân, 4/334. )`,
+      bg: `#b22222`,
+
+},
+{
+      hadis: `İman, yetmiş küsur derecedir. En üstünü "Lâ ilâhe illallah (Allah'tan başka ilah yoktur)" sözüdür, en düşük derecesi de rahatsız edici bir şeyi yoldan kaldırmaktır. Haya da imandandır.`,
+      kaynak: `(Buhârî, Îmân, 3; Müslim, Îmân, 57, 58. )`,
+      bg: `#b22222`,
+
+},
+{
+      hadis: `Kim kötü ve çirkin bir iş görürse onu eliyle düzeltsin; eğer buna gücü yetmiyorsa diliyle düzeltsin; buna da gücü yetmezse, kalben karşı koysun. Bu da imanın en zayıf derecesidir.`,
+      kaynak: `(Müslim, Îmân, 78; Ebû Dâvûd, Salât, 248. )`,
+      bg: `#b22222`,
+
+},
+{
+      hadis: `İki göz vardır ki, cehennem ateşi onlara dokunmaz: Allah korkusundan ağlayan göz, bir de gecesini Allah yolunda, nöbet tutarak geçiren göz.`,
+      kaynak: `(Tirmizî, Fedâilü'l-Cihâd, 12. )`,
+      bg: `#b22222`,
+
+},
+{
+      hadis: `Zarar vermek ve zarara zararla karşılık vermek yoktur.`,
+      kaynak: `(İbn Mâce, Ahkâm, 17; Muvatta', Akdıye, 31. )`,
+      bg: `#b22222`,
+
+},
+{
+      hadis: `Hiçbiriniz kendisi için istediğini (mü'min) kardeşi için istemedikçe (gerçek) iman etmiş olamaz.`,
+      kaynak: `(Buhârî, Îmân, 7; Müslim, Îmân, 71. )`,
+      bg: `#b22222`,
+
+},
+{
+      hadis: `Müslüman müslümanın kardeşidir. Ona zulmetmez, onu (düşmanına) teslim etmez. Kim, (mümin) kardeşinin bir ihtiyacını giderirse Allah da onun bir ihtiyacını giderir. Kim müslümanı bir sıkıntıdan kurtarırsa, bu sebeple Allah da onu kıyamet günü sıkıntılarının birinden kurtarır. Kim bir müslümanı(n kusurunu) örterse, Allah da Kıyamet günü onu(n kusurunu) örter.`,
+      kaynak: `(Buhârî, Mezâlim, 3; Müslim, Birr, 58. )`,
+      bg: `#b22222`,
+
+},
+{
+      hadis: `İman etmedikçe cennete giremezsiniz, birbirinizi sevmedikçe de (gerçek anlamda) iman etmiş olamazsınız.`,
+      kaynak: `(Müslim, Îmân, 93; Tirmizî, Sıfâtu'l-Kıyâme, 56. )`,
+      bg: `#b22222`,
+
+},
+{
+      hadis: `Müslüman, insanların elinden ve dilinden emin olduğu kimsedir.`,
+      kaynak: `(Tirmizî, Îmân, 12; Nesâî, Îmân, 8. )`,
+},
+{
+      hadis: `Birbirinize buğuz etmeyin, birbirinize haset etmeyin, birbirinize arka çevirmeyin; ey Allah'ın kulları, kardeş olun. Bir müslümana, üç günden fazla (din) kardeşi ile dargın durması helal olmaz.`,
+      kaynak: `(Buhârî, Edeb, 57, 58. )`,
+},
+{
+      hadis: `Hiç şüphe yok ki doğruluk iyiliğe götürür. İyilik de cennete götürür. Kişi doğru söyleye söyleye Allah katında sıddîk (doğru sözlü) diye yazılır. Yalancılık kötüye götürür. Kötülük de cehenneme götürür. Kişi yalan söyleye söyleye Allah katında kezzâb (çok yalancı) diye yazılır. `,
+      kaynak: `(Buhârî, Edeb, 69; Müslim, Birr, 103, 104. )`,
+},
+{
+      hadis: `(Mümin) kardeşinle münakaşa etme, onun hoşuna gitmeyecek şakalar yapma ve ona yerine getirmeyeceğin bir söz verme. `,
+      kaynak: `(Tirmizî, Birr, 58. )`,
+},
+{
+      hadis: `(Mümin) kardeşine tebessüm etmen sadakadır. İyiliği emredip kötülükten sakındırman sadakadır. Yolunu kaybeden kimseye yol göstermen sadakadır. Yoldan taş, diken, kemik gibi şeyleri kaldırıp atman da senin için sadakadır.`,
+      kaynak: `(Tirmizî, Birr, 36. )`,
+},
+{
+      hadis: `Allah sizin ne dış görünüşünüze ne de mallarınıza bakar. Ama o sizin kalplerinize ve işlerinize bakar.`,
+      kaynak: `(Müslim, Birr, 33; ‹bn Mâce, Zühd, 9; Ahmed b. Hanbel, 2/285, 539. )`,
+},
+{
+      hadis: `Allah'ın rızası, anne ve babanın rızasındadır. Allah'ın öfkesi de anne babanın öfkesindedir. `,
+      kaynak: `(Tirmizî, Birr, 3. )`,
+},
+{
+      hadis: `Üç dua vardır ki, bunlar şüphesiz kabul edilir:
+      Mazlumun duası, misafirin duası ve babanın evladına duası.`,
+      kaynak: `(İbn Mâce, Dua, 11. )`,
+},
+{
+      hadis: `Hiçbir baba, çocuğuna, güzel terbiyeden daha üstün bir hediye veremez.`,
+      kaynak: `(Tirmizî, Birr, 33. )`,
+},
+{
+      hadis: `Sizin en hayırlılarınız, hanımlarına karşı en iyi davrananlarınızdır. `,
+      kaynak: `(Tirmizî, Radâ', 11; ‹bn Mâce, Nikâh, 50. )`,
+},
+{
+      hadis: `Küçüklerimize merhamet etmeyen, büyüklerimize saygı göstermeyen bizden değildir.`,
+      kaynak: `(Tirmizî, Birr, 15; Ebû Dâvûd, Edeb, 66. )`,
+},
+{
+      hadis: `Peygamberimiz işaret parmağı ve orta parmağıyla işaret ederek: "Gerek kendisine ve gerekse başkasına ait herhangi bir yetimi görüp gözetmeyi üzerine alan kimse ile ben, cennette işte böyle yanyanayız" buyurmuştur.`,
+      kaynak: `(Buhârî, Talâk, 25, Edeb, 24; Müslim, Zühd, 42. )`,
+},
+{
+      hadis: `(İnsanı) helâk eden şu yedi şeyden kaçının. Onlar nelerdir ya Resulullah dediler. Bunun üzerine: Allah'a şirk koşmak, sihir, Allah'ın haram kıldığı cana kıymak, faiz yemek, yetim malı yemek, savaştan kaçmak, suçsuz ve namuslu mümin kadınlara iftirada bulunmak buyurdu. `,
+      kaynak: `(Buhârî, Vasâyâ, 23, Tıbb, 48; Müslim, Îmân, 144. )`,
+},
+{
+      hadis: `Allah'a ve ahiret gününe imân eden kimse, komşusuna eziyet etmesin. Allah'a ve ahiret gününe imân eden misafirine ikramda bulunsun. Allah'a ve ahiret gününe imân eden kimse, ya hayır söylesin veya sussun. `,
+      kaynak: `(Buhârî, Edeb, 31, 85; Müslim, Îmân, 74, 75. )`,
+},
+{
+      hadis: `Cebrâil bana komşu hakkında o kadar çok tavsiyede bulundu ki; ben (Allah Teâlâ) komşuyu komşuya mirasçı kılacak zannettim. `,
+      kaynak: `(Buhârî, Edeb, 28; Müslim, Birr, 140, 141. )`,
+},
+{
+      hadis: `Dul ve fakirlere yardım eden kimse, Allah yolunda cihad eden veya gündüzleri (nafile) oruç tutup, gecelerini (nafile) ibadetle geçiren kimse gibidir. `,
+      kaynak: `(Buhârî, Nafakât, 1; Müslim, Zühd, 41; Tirmizî, Birr, 44; Nesâî, Zekât, 78. )`,
+},
+{
+      hadis: `Her insan hata eder. Hata işleyenlerin en hayırlıları tevbe edenlerdir. `,
+      kaynak: `(Tirmizî, Kıyâme, 49; İbn Mâce, Zühd, 30. )`,
+},
+{
+      hadis: `Mü'minin başka hiç kimsede bulunmayan ilginç bir hali vardır; O'nun her işi hayırdır. Eğer bir genişliğe (nimete) kavuşursa şükreder ve bu onun için bir hayır olur. Eğer bir darlığa (musibete) uğrarsa sabreder ve bu da onun için bir hayır olur. `,
+      kaynak: `(Müslim, Zühd, 64; Dârim", Rikâk, 61. )`,
+},
+{
+      hadis: `Bizi aldatan bizden değildir. `,
+      kaynak: `(Müslim, Îmân, 164. )`,
+},
+{
+      hadis: `Söz taşıyanlar (cezalarını çekmeden ya da affedilmedikçe) cennete giremezler. `,
+      kaynak: `(Müslim, Îmân, 168; Tirmizî, Birr, 79. )`,
+},
+{
+      hadis: `İşçiye ücretini, (alnının) teri kurumadan veriniz. `,
+      kaynak: `(İbn Mâce, Ruhûn, 4. )`,
+},
+{
+      hadis: `Bir müslümanın diktiği ağaçtan veya ektiği ekinden insan, hayvan ve kuşların yedikleri şeyler, o müslüman için birer sadakadır. `,
+      kaynak: `(Buhârî, Edeb, 27; Müslim, Müsâkât, 7, 10. )`,
+},
+{
+      hadis: `İnsanda bir organ vardır. Eğer o sağlıklı ise bütün vücut sağlıklı olur; eğer o bozulursa bütün vücut bozulur. Dikkat edin! O, kalptir. `,
+      kaynak: `(Buhârî, Îmân, 39; Müslim, Müsâkât, 107. )`,
+},
+{
+      hadis: `Rabbinize karşı gelmekten sakının, beş vakit namazınızı kılın, Ramazan orucunuzu tutun, mallarınızın zekatını verin, yöneticilerinize itaat edin. (Böylelikle) Rabbinizin cennetine girersiniz. `,
+      kaynak: `(Tirmizî, Cum'a, 80.)`,
+}
+];
+        
+var renkler = [
+  {
+    bg: `https://i.hizliresim.com/boiwmzo.jpg`,
+    color: `#fff`
+  },
+  {
+    bg: `https://i.hizliresim.com/tuzjulq.jpg`,
+    color: `#fff`
+  },
+  {
+    bg: `https://i.hizliresim.com/3fxbrds.jpg`,
+    color: `#fff`
+  },
+  {
+    bg: `https://i.hizliresim.com/m39ho8o.jpg`,
+    color: `#fff`
+  },
+  {
+    bg: `https://i.hizliresim.com/3vn88h1.jpg`,
+    color: `#fff`
+  },
+  {
+    bg: `https://i.hizliresim.com/s5t0kdq.jpg`,
+    color: `#fff`
+  },
+  {
+    bg: `https://i.hizliresim.com/7gjyddn.jpg`,
+    color: `#fff`
+  },
+  {
+    bg: `https://i.hizliresim.com/bffovzh.jpg`,
+    color: `#fff`
+  },
+  {
+    bg: `https://i.hizliresim.com/nwwok02.jpg`,
+    color: `#fff`
+  },
+  {
+    bg: `https://i.hizliresim.com/jxaj0qr.jpg`,
+    color: `#fff`
+  },
+  {
+    bg: `https://i.hizliresim.com/4dvx49o.jpg`,
+    color: `#fff`
+  },
+  {
+    bg: `https://i.hizliresim.com/8mjv2d8.jpg`,
+    color: `#fff`
+  },
+  {
+    bg: `https://i.hizliresim.com/r50u6qj.jpg`,
+    color: `#fff`
+  },
+  {
+    bg: `https://i.hizliresim.com/1d8h5m7.jpg`,
+    color: `#fff`
+  },
+  {
+    bg: `https://i.hizliresim.com/f92i66r.jpg`,
+    color: `largefont`
+  },
+  {
+    bg: `https://i.hizliresim.com/s1duvc6.jpg`,
+    color: `#fff`
+  },
+  {
+    bg: `https://i.hizliresim.com/mmoforx.jpg`,
+    color: `#fff`
+  },
+  {
+    bg: `https://i.hizliresim.com/39ln5db.jpg`,
+    color: `#fff`
+  },
+  {
+    bg: `https://i.hizliresim.com/hubvbe6.jpg`,
+    color: `#fff`
+  },
+  {
+    bg: `https://i.hizliresim.com/00ced1`,
+    color: `#fff`
+  },
+  {
+    bg: `https://i.hizliresim.com/40e0d0`,
+    color: `#fff`
+  },
+  {
+    bg: `https://i.hizliresim.com/00ffff`,
+    color: `#fff`
+  },
+  {
+    bg: `https://i.hizliresim.com/7fffd4`,
+    color: `#fff`
+  },
+  {
+    bg: `https://i.hizliresim.com/76eec6`,
+    color: `#fff`
+  },
+  {
+    bg: `https://i.hizliresim.com/66cdaa`,
+    color: `#fff`
+  },
+  {
+    bg: `https://i.hizliresim.com/54ff9f`,
+    color: `#fff`
+  },
+  {
+    bg: `https://i.hizliresim.com/4eee94`,
+    color: `#fff`
+  },
+  {
+    bg: `https://i.hizliresim.com/00ff7f`,
+    color: `#fff`
+  },
+  {
+    bg: `https://i.hizliresim.com/00ee76`,
+    color: `#fff`
+  },
+  {
+    bg: `https://i.hizliresim.com/c0ff3e`,
+    color: `#fff`
+  },
+  {
+    bg: `https://i.hizliresim.com/b3ee3a`,
+    color: `#fff`
+  },
+  {
+    bg: `https://i.hizliresim.com/9acd32`,
+    color: `#fff`
+  },
+  {
+    bg: `https://i.hizliresim.com/ffd700`,
+    color: `#fff`
+  },
+  {
+    bg: `#eec900`,
+    color: `#fff`
+  },
+  {
+    bg: `#ffe413`,
+    color: `#fff`
+  },
+  {
+      bg: `#758BF9`,
+      color: `#fff`
+    },
+    {
+      bg: `#6076E5`,
+      color: `#fff`
+    },
+    {
+      bg: `#5066D3`,
+      color: `#fff`
+    },
+    {
+      bg: `#4056BC`,
+      color: `#fff`
+    },
+    {
+      bg: `#2F44A3`,
+      color: `#fff`
+    }
+];
+
+
+var index = 0;
+var containerDiv = document.getElementById("container");
+var hadisDiv = document.getElementById("hadis");
+var turkce = document.querySelector(".turkce")
+var idturkce = document.getElementById("turkce")
+var sag = document.querySelector(".fa-chevron-right")
+var sol = document.querySelector(".fa-chevron-left")
+
+function showHadis() {
+   
+      var currentHadis = hadisler[index];
+    hadisDiv.innerHTML = `
+            <h3 class="turkce" id="turkce">${currentHadis.hadis} <br><br> <span class="kaynak">${currentHadis.kaynak}</span></h3>
+    `;
+    if(renkler[index].color == `largefont`){
+      idturkce.classList.add("largefont")
+    }
+    containerDiv.style.backgroundImage = `url(${renkler[index].bg})`;
+    
+    sag.style.color = renkler[index].color;
+    sol.style.color = renkler[index].color;
+    hadisDiv.classList.remove("fade-out");
+    hadisDiv.classList.remove("fade-in");
+    void hadisDiv.offsetWidth;
+    hadisDiv.classList.add("fade-in");
+}
+
+function hideHadis() {
+    hadisDiv.classList.remove("fade-in");
+    hadisDiv.classList.add("fade-out");
+}
+
+function nextHadis() {
+    hideHadis();
+    setTimeout(function() {
+        if (index < hadisler.length - 1) {
+            index++;
+        } else {
+            index = 0;
+        }
+        showHadis();
+    }, 500);
+}
+
+function previousHadis() {
+    hideHadis();
+    setTimeout(function() {
+        if (index > 0) {
+            index--;
+        } else {
+            index = hadisler.length - 1;
+        }
+        showHadis();
+    }, 500);
+}
+
+// İlk hadisi göstermek için
+showHadis();
+
+
+function beyazla(){
+      document.querySelector(".turkce").style.color  = "white"
+}
+function siyahla(){
+      document.querySelector(".turkce").style.color  = "black"
+}
